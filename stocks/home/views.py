7cc0a89817755bc64321b29f7a8e4eca565fa728
AMRENDRA SINGH <ashking13th@gmail.com>
2018-05-28 00:34:33 +0530

    Add files via upload

--- stocks/home/views.py
+++ stocks/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Yaay. My home page</h1>")
+    template = loader.get_template('home/orange/index.html')
+    return HttpResponse(template.render())
